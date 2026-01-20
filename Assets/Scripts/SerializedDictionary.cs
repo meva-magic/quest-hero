17cedbffb394f5a7947925d286a6d0456f3f43c3
saharov-dev <saharov.dev@gmail.com>
2026-01-20 15:56:53 +0300

    🐝

---
+++ Assets/Scripts/SerializedDictionary.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class SerializedDictionary<TKey, TValue> : Dictionary<TKey, TValue>, ISerializationCallbackReceiver
+{
+    [SerializeField]
+    private List<TKey> keys = new List<TKey>();
+    
+    [SerializeField]
+    private List<TValue> values = new List<TValue>();
+    
+    // Save the dictionary to lists
+    public void OnBeforeSerialize()
+    {
+        keys.Clear();
+        values.Clear();
+        
+        foreach (var pair in this)
+        {
+            keys.Add(pair.Key);
+            values.Add(pair.Value);
+        }
+    }
+    
+    // Load the dictionary from lists
+    public void OnAfterDeserialize()
+    {
+        this.Clear();
+        
+        if (keys.Count != values.Count)
+        {
+            Debug.LogError($"Mismatch in key/value counts: {keys.Count} keys, {values.Count} values");
+        }
+        
+        for (int i = 0; i < Math.Min(keys.Count, values.Count); i++)
+        {
+            this[keys[i]] = values[i];
+        }
+    }
+}
